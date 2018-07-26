2b5f123091d9510b9f7ebf0755d261b382e15816
Robin Douglas <robin.douglas@trimble.com>
2018-07-26 14:26:38 +0100

    Add basic hotkeys

--- main.go
+++ main.go
@@ -6,38 +6,88 @@ import (
 	"os"
 	"os/signal"
 	"sync"
+	"unsafe"
+	sys "golang.org/x/sys/windows"
 
 	"github.com/robin1996/go-macro/mouse"
+	"github.com/moutend/go-hook"
 )
 
+type HotKey struct {
+	Id int
+	Modifiers int
+	KeyCode int
+}
+
+type MSG struct {
+	HWND   uintptr
+	UINT   uintptr
+	WPARAM int16
+	LPARAM int64
+	DWORD  int32
+	hook.POINT
+}
+
 func main() {
-	fmt.Println("start capturing mouse input")
+	user32 := sys.MustLoadDLL("user32")
+	defer user32.Release()
 
-	var isInterrupted bool
-	var wg sync.WaitGroup
+	regHotKey := user32.MustFindProc("RegisterHotKey")
+	peekMsg := user32.MustFindProc("PeekMessageW")
+	
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	ctx, cancel := context.WithCancel(context.Background())
-	mouseChan := make(chan mouse.MouseMessage, 1)
+	hotKeys := map[int16]*HotKey {
+		1: &HotKey{1, 0, 0x78}, // F9 -- See https://docs.microsoft.com/en-us/windows/desktop/inputdev/virtual-key-codes
+		2: &HotKey{2, 0, 0x79}, // F10
+	}
 
-	go func() {
-		wg.Add(1)
-		mouse.Notify(ctx, mouseChan)
-		wg.Done()
-	}()
-	for {
-		if isInterrupted {
-			cancel()
-			break
-		}
-		select {
-		case <-signalChan:
-			isInterrupted = true
-		case k := <-mouseChan:
-			fmt.Println(k.Button, k.POINT.X, k.POINT.Y)
+	for _, v := range hotKeys {
+		r1, _, err := regHotKey.Call(
+			0, uintptr(v.Id), uintptr(v.Modifiers), uintptr(v.KeyCode))
+		if r1 != 1 {
+			fmt.Println("Failed to register", v, ", error:", err)
 		}
 	}
-	wg.Wait()
-	fmt.Println("done")
+
+	fmt.Println("Press F9 to start!")
+
+	for {
+		var msg = &MSG{}
+		peekMsg.Call(uintptr(unsafe.Pointer(msg)), 0, 0, 0, 1)
+
+		if id := msg.WPARAM; id != 0 {
+			if id == 1 {
+				fmt.Println("start capturing mouse input")
+
+				var isInterrupted bool
+				var wg sync.WaitGroup
+
+				signalChan := make(chan os.Signal, 1)
+				signal.Notify(signalChan, os.Interrupt)
+				ctx, cancel := context.WithCancel(context.Background())
+				mouseChan := make(chan mouse.MouseMessage, 1)
+
+				go func() {
+					wg.Add(1)
+					mouse.Notify(ctx, mouseChan)
+					wg.Done()
+				}()
+				for {
+					if isInterrupted {
+						cancel()
+						break
+					}
+					select {
+					case <-signalChan:
+						isInterrupted = true
+					case k := <-mouseChan:
+						fmt.Println(k.Button, k.POINT.X, k.POINT.Y)
+					}
+				}
+				wg.Wait()
+				fmt.Println("done")
+				return
+			}
+		}
+	}
 }
