4162b572274b31c269b4d7b4254700921aa76dfc
Robin Douglas <robin.douglas@trimble.com>
2018-07-30 11:51:25 +0100

    Add play sequence

--- cmd/gmp.go
+++ cmd/gmp.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func playStartSeq() {
+	fmt.Println("Playing macro in...")
+	time.Sleep(time.Second)
+	fmt.Println("3...")
+	time.Sleep(time.Second)
+	fmt.Println("2...")
+	time.Sleep(time.Second)
+	fmt.Println("1...")
+	time.Sleep(time.Second)
+	fmt.Println("GO!")
+}
 
 func main() {
-	fmt.Println("vim-go")
+	playStartSeq()
 }
